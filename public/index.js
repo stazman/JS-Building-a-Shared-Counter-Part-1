2b31e07412742557777c36e7eca466185b6f5713
Christopher DiStasio <cadistasio@gmail.com>
2022-10-14 21:21:03 -0500

    Solves activity and bonus

--- public/index.js
+++ public/index.js
@@ -1,9 +1,16 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#refresh-button');
 
-    let countValue = 0;
+    let counterStart = await fetch('http://127.0.0.1:9001/counter')
+
+    let result = await counterStart.json();
+
+    countValue = result.value;
+
+    // console.log(countValue)
 
     function increment(){
         countValue++;
@@ -15,8 +22,22 @@ function main(){
         countContainer.textContent = countValue;
     }
 
+    async function refresh(){
+        let counterStart = await fetch('http://127.0.0.1:9001/counter')
+
+        let result = await counterStart.json();
+
+        countValue = result.value;
+
+        countContainer.textContent = countValue;
+
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refresh)
     countContainer.textContent = countValue;
+
 }
+
 main()
